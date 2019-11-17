fe6300cf730423c0471ef0c0baae21777397af48
Listiarini <lis.listiarini@gmail.com>
2019-11-16 23:39:20 -0800

    Fix database6

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/login'))
     .get('/main', (req, res) => res.render('pages/NotAWebApp'))
-    .get('/menu', (req, res) => res.render('pages/mainmenu'))
+    .get('/mainmenu', (req, res) => res.render('pages/mainmenu'))
     .get('/underconstruction', (req, res) => res.render('pages/underconstruction'))
     .get('/login', (req, res) => res.render('pages/login'))
     .post('/login', async(req, res) => {
